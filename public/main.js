1d242af2fd4d1034ed3a65b9b7a270f2fb7543c5
Mahaon17 <89358047+Mahaon17@users.noreply.github.com>
2022-06-27 02:30:10 +0300

    Creating a new variable

--- public/main.js
+++ public/main.js
@@ -1,13 +1,16 @@
 const area = document.getElementById('area');
 let move = 0;
+let result = '';
 
 area.addEventListener('click', e => {
     if(e.target.className = 'box') {
         move % 2 === 0 ? e.target.innerHTML = 'X' : e.target.innerHTML = 'O';
         move++;
+        check();
     }
 } 
 )
+
 
 const array = [
     [0,1,2],
